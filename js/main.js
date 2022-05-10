bf90e43bbffcb8def30659f189fa9cbc7c64ba0d
Samtray <sammychurro@gmail.com>
2022-05-09 23:49:36 -0700

    Finished fake Auth service

--- js/main.js
+++ js/main.js
@@ -4,22 +4,42 @@ import { auth } from './services/user.js';
 //events
 window.addEventListener('load', () => { init(); });
 
+
+//Variables
+const SUCCESS = "#4CAF50";
+const FAILED = "#FD8e8e";
+
 function init() {
     console.log('Initializing document...');
     document.getElementById('button-sign-in').addEventListener('click', () => { login() });
 }
 
+function showMessage(status, result){
+    let message = document.getElementById('message');
+    message.innerHTML = result.message
+    message.style.backgroundColor = status;
+    message.style.visibility = "visible";
+    message.style.animation = "fadeIn 0.2s"
+    // setTimeout(() => {
+    //     message.style.animation = "fadeOut 0.1s"
+    //     setTimeout(() => {
+    //         message.style.visibility = "hidden";
+    //     }, 100)
+    // }, 4000)
+}
+
+
 //login
 function login() {
     console.log('Login...')
-    auth('admin', 'password').then((result) => {
-        let message = document.getElementById('message');
-        if (result.status = 0)
-            message.innerHTML = result.message
-        message.style.backgroundColor = "#4CAF50";
-        message.style.visibility = "visible";
-        setTimeout(() => {
-            document.getElementById('message').style.visibility = "hidden";
-        }, 1000)
+    let username = document.getElementById('input-username').value;
+    let password = document.getElementById('input-password').value;
+    auth(username, password).then((result) => {
+        switch (result.status){
+            case 0: showMessage(SUCCESS, result); break;
+            case 1: showMessage(FAILED, result); break;
+            default: showMessage(FAILED, result); break;
+        }
+           
     });
 }
