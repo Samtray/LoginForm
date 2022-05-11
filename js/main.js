7cbba4f68b875a9825b7287316c4ab111837d6c6
Samtray <sammychurro@gmail.com>
2022-05-10 17:55:33 -0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -6,8 +6,8 @@ window.addEventListener('load', () => { init(); });
 
 
 //Variables
-const SUCCESS = "#4CAF50";
-const FAILED = "#FD8e8e";
+const SUCCESS = "#68E06C";
+const FAILED = "#FD8E8E";
 
 function init() {
     console.log('Initializing document...');
@@ -20,20 +20,15 @@ function showMessage(status, result){
     message.style.backgroundColor = status;
     message.style.visibility = "visible";
     message.style.animation = "fadeIn 0.2s"
-    // setTimeout(() => {
-    //     message.style.animation = "fadeOut 0.1s"
-    //     setTimeout(() => {
-    //         message.style.visibility = "hidden";
-    //     }, 100)
-    // }, 4000)
-}
 
+}
 
 //login
 function login() {
     console.log('Login...')
     let username = document.getElementById('input-username').value;
     let password = document.getElementById('input-password').value;
+
     auth(username, password).then((result) => {
         switch (result.status){
             case 0: showMessage(SUCCESS, result); break;
